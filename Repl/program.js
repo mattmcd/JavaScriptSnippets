f8cb4858b16281059c1b90fe2308f82dea7fb5a8
mattmcd <matt@matt-mcdonnell.com>
2015-08-31 17:29:10 +0100

    Refactor with intention of adding state for command prompt.

--- Repl/program.js
+++ Repl/program.js
@@ -1,19 +1,27 @@
+function make_repl(node) {
+  var readChar = function(evt) {
+    var cmd = node.value;
+    var startInd = node.selectionStart - 1;
+    var endInd = node.selectionEnd;
+    var c = cmd.slice(startInd, endInd);
+    console.log('Read a character: ' + c);
+    // Example character dependent processing: ignore 'd'
+    if (c === 'd') {
+      node.value = cmd.slice(0, startInd) + cmd.slice(endInd); 
+      node.selectionStart = startInd;
+      node.selectionEnd = startInd;
+    }
+  }
+
+  node.oninput = readChar;
+  return {readChar: readChar};
+}
+
+
 function init() {
   console.log('Loaded');
   var cmdline = document.getElementById('cmdline');
-  cmdline.oninput = readChar;
+  // cmdline.oninput = readChar;
+  var repl = make_repl(cmdline);
 }
 
-function readChar(evt) {
-  var cmd = evt.target.value;
-  var startInd = evt.target.selectionStart - 1;
-  var endInd = evt.target.selectionEnd;
-  var c = cmd.slice(startInd, endInd);
-  console.log('Read a character: ' + c);
-  // Example character dependent processing: ignore 'd'
-  if (c === 'd') {
-    evt.target.value = cmd.slice(0, startInd) + cmd.slice(endInd); 
-    evt.target.selectionStart = startInd;
-    evt.target.selectionEnd = startInd;
-  }
-}
