266624baecc7bffbc47d6effcc4e81cffd0e190a
mattmcd <matt@matt-mcdonnell.com>
2015-09-09 15:19:07 +0100

    Split js code into objects and demo files.

--- ImageSource/video.js
+++ ImageSource/video.js
@@ -121,15 +121,3 @@ function make_image_source_view(image_source) {
   }
 
 }
-
-var view;
-var view2;
-
-function pageDidLoad() {
-  var image_source = make_image_source(true);
-  view = make_image_source_view(image_source);
-  view.init("live", "display", "camera");
-  // Demo: two views
-  view2 = make_image_source_view(image_source);
-  view2.init("live2", "display2", "camera2");
-}
